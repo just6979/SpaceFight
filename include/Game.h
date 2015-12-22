34720fb49d6b24d6a7c912a712053c6fd8d7ee37
Justin White <jw@justinwhite.net>
2015-12-22 02:06:16 -0500

    Code formatting.

--- include/Game.h
+++ include/Game.h
@@ -16,11 +16,8 @@
 class Game {
 public:
     static Game *getGame();
-
     void init(const std::string &_name, unsigned int _width, unsigned int _height);
-
     void run();
-
 private:
     static Game *instance;
 
@@ -37,7 +34,6 @@ private:
     const float deadZone = 15;
     const float keySpeed = 75;
     GameSprite *player;
-
 // system functions
     void resizeWindow(bool fullscreen);
     void adjustScale();
