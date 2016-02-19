e338e3ac392faa3f96181951fbe2f774e44aa596
Justin White <jw@justinwhite.net>
2016-02-19 16:18:34 -0500

    FIx player positioning.

--- src/GameSprite.cpp
+++ src/GameSprite.cpp
@@ -27,13 +27,15 @@ void GameSprite::setTexture(const sf::Image& _image) {
 void GameSprite::setVertices() {
     sf::Vector2u size = texture.getSize();
 
+    setOrigin(size.x / 2, size.y / 2);
+
     vertices.setPrimitiveType(sf::Quads);
     vertices.resize(4);
 
-    vertices[0].position = sf::Vector2f(-size.x / 2, -size.y / 2);
-    vertices[1].position = sf::Vector2f(size.x / 2, -size.y / 2);
-    vertices[2].position = sf::Vector2f(size.x / 2, size.y / 2);
-    vertices[3].position = sf::Vector2f(-size.x / 2, size.y / 2);
+    vertices[0].position = sf::Vector2f(0, 0);
+    vertices[1].position = sf::Vector2f(0, size.y);
+    vertices[2].position = sf::Vector2f(size.x, size.y);
+    vertices[3].position = sf::Vector2f(size.x, 0);
 
     vertices[0].texCoords = sf::Vector2f(0, 0);
     vertices[1].texCoords = sf::Vector2f(size.x, 0);
